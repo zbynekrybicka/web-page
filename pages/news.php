6d94978814fa1966863732b55cf91fa6aea52086
Zbynek Rybicka <zbynek.rybicka@gmail.com>
2025-11-22 22:07:50 +0100

    Programátor je řemeslník

--- pages/news.php
+++ pages/news.php
@@ -1,9 +1,18 @@
 <h1>News</h1>
 
 <div class="activity-feed">
-    <h2>Špagety-kód je můj kamarád: Cesta k nejobtížnější, ale nejvíce ceněné specializaci
-        <div class="activity-date">22. 11. 2025</div>
-    </h2>
+    <h2><a href="/programator-je-remeslnik">⛏️ Programátor je řemeslník: Proč musíme striktně oddělit výzkum od placené praxe</a>  <div class="activity-date">23. 11. 2025</div></h2>
+
+    <h3>🔬 Laboratoř vs. Dílna: Kde leží hranice profesionální odpovědnosti</h3>
+
+    <p>Každý programátor, kterého znám, má v sobě skrytou laboratoř. Místo, kde se rodí nápady, kde se testují šílené architektury a zkouší neprověřené frameworky. Tato vášeň pro objevování je palivem, které žene náš obor kupředu. Ale právě zde, na pomezí osobního rozvoje a profesionální zakázky, musíme vést nekompromisní čáru. Jakmile na stůl dopadne smlouva s klientem a začnou běžet hodiny, laboratoř se musí zavřít. Nyní nastupuje <b>dílna</b>, kde vládne prediktabilita a ověřené postupy.
+        <a href="/programator-je-remeslnik" class="read-more-link">Více...</a>
+    </p>
+
+</div>
+
+<div class="activity-feed">
+    <h2><a href="/spagety-kod-je-muj-kamarad">Špagety-kód je můj kamarád: Cesta k nejobtížnější, ale nejvíce ceněné specializaci</a> <div class="activity-date">22. 11. 2025</div></h2>
     
 
     <h3>​Vítejte v Zóně Nechtěných</h3>
