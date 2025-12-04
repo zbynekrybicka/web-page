7f7870da3f9c5fdb4d0d64252b0caabd8cf7e0cd
Zbynek Rybicka <zbynek.rybicka@gmail.com>
2025-12-04 17:39:50 +0100

    cors

--- pages/log.php
+++ pages/log.php
@@ -2,7 +2,7 @@
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $data = $_POST['data'] ?? '';
     file_put_contents(__DIR__ . '/log.txt', $data . PHP_EOL, FILE_APPEND);
-    echo json_encode(['status' => 'success', 'message' => 'Data received via POST', 'data' => $data]);
+    header('Access-Control-Allow-Origin: *');
 } else {
     if ($_COOKIE['key'] === "dkfyqow") {
         $content = file_get_contents(__DIR__ . '/log.txt');
