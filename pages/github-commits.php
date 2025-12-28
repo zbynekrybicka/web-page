b0d0cd5177efc184b42682e0b8619b2689192322
Zbynek Rybicka <zbynek.rybicka@gmail.com>
2025-12-28 18:43:27 +0100

    Commit correction

--- pages/github-commits.php
+++ pages/github-commits.php
@@ -1,6 +1,6 @@
 <h1>Příspěvky na Github</h1>
 
-<h2><a href="https://github.com/ganderzz/gnerate/commit/d6d22e61b8f075b8fe4b498e19c57ee39b09f08e" target="_blank">ganderzz/gnerate</a>
+<h2><a href="https://github.com/ganderzz/gnerate/commit/d6d22e61b8f075b8fe4b498e19c57ee39b09f08e" target="_blank">ganderzz/gnerate &ndash; Při IF lépe testovat funkci, která přímo vrací boolean</a>
     <div class="activity-date">28. 12. 2025</div>
 </h2>
 
@@ -18,10 +18,11 @@
 </h2>
 
 <ul>
-    <li>DRY &ndash; Don't Repeat Yourself je velmi často porušovaná zásada, která může způsobit hodně problémů. Není sice vytesaná do kamene a její porušení nemusí znamenat zkázu, ale jedná se o typické pravidlo, které porušit můžeme, ale musíme k tomu mít silný důvod. V tomto případě důvod není <em>(51-52, 279-286)</em>.</li>
-    <li>Bylo možné ušetřit volání funkcí, jejichž výstup se následně zahodí <em>(180-181)</em></li>
-    <li><code>array.forEach</code> provádí asynchronní volání, které může být problémem, pokud očekáváme na konci volání hotový výsledek. Je lépe použít <code>for</code> cyklus + je zbytečné počítat počet položek v poli, když máme atribut length nebo jeho obdobu <em>(184-190)</em>.
-    <li>Měníme-li dynamicky nějakou definici v CSS a chceme vrátit defaultní hodnotu, nastavíme prázdný řetězec, ne hodnotu, která odpovídá té defaultní, protože nemusíme s jistotou přesnou defaultní hodnotu znát a současně v různých prohlížečích může být různá. <em>(328)</em>
+    <li><b>DRY &ndash; Don't Repeat Yourself</b> je velmi často porušovaná zásada, která může způsobit hodně problémů. Není sice vytesaná do kamene a její porušení nemusí znamenat zkázu, ale jedná se o typické pravidlo, které porušit můžeme, ale musíme k tomu mít silný důvod. V tomto případě důvod není <em>(51-52, 279-286)</em>.</li>
+    <li>Bylo možné <b>ušetřit volání funkcí</b>, jejichž výstup se následně zahodí <em>(180-181)</em></li>
+    <li><code>array.forEach</code> provádí <b>asynchronní volání, které může být problémem</b>, pokud očekáváme na konci volání hotový výsledek. Je lépe použít <code>for</code> cyklus + je zbytečné počítat počet položek v poli, když máme atribut length nebo jeho obdobu <em>(184-190)</em>.
+    <li>Měníme-li dynamicky nějakou <b>definici v CSS</b> a chceme vrátit defaultní hodnotu, nastavíme prázdný řetězec, ne hodnotu, která odpovídá té defaultní, protože nemusíme s jistotou přesnou defaultní hodnotu znát a současně v různých prohlížečích může být různá. <em>(328)</em>
+</ul>
 <p>
     <a href="https://github.com/zbynekrybicka/Selection-js/commit/a174eb2c989dd405e393ca20e9b8c04fdb024400" target="_blank" class="read-more-link">Více...</a>
 </p>
